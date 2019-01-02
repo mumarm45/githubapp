603742dbe44f6828b8e267d45a6b6da4836b6b26
Muhammad Omar Muneer <muhammad.umar@expertflow.com>
2019-01-02 12:03:22 +0400

    Updates

---
+++ precache-manifest.39e4d1a6ba2861f92b07c45ca49a05e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18c7192dbb5eb9f53405",
+    "url": "/githubapp/static/css/main.a662d86c.chunk.css"
+  },
+  {
+    "revision": "18c7192dbb5eb9f53405",
+    "url": "/githubapp/static/js/main.18c7192d.chunk.js"
+  },
+  {
+    "revision": "89d30a294e1f8d5dd246",
+    "url": "/githubapp/static/js/1.89d30a29.chunk.js"
+  },
+  {
+    "revision": "fc6e7a2c97669a4f2271",
+    "url": "/githubapp/static/js/runtime~main.fc6e7a2c.js"
+  },
+  {
+    "revision": "96da2a400c6bd3144d86e58e848498cc",
+    "url": "/githubapp/index.html"
+  }
+];
